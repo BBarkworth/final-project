cd226e47b1c72991fdd1f6b8418d4e4d5c9a68a0
Benjamin Barkworth <benjaminbarkworth@Benjamins-MacBook-Air.local>
2022-11-18 16:40:53 +0000

    Complete functions

--- tools.py
+++ tools.py
@@ -2,8 +2,6 @@ import requests
 # library for working with APIs
 import json 
 # library for working with JSON data
-import csv
-# for reading csv files
 import sqlite3
 # for working with sqlite - alternative to using CS50 library
 # check line 493 exercise on exercises for how to add SQL table
@@ -22,18 +20,6 @@ def test(obj):
 
 #teams_response = requests.get("https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams?limit=32")
     # use teams csv instead??
-
-
-scores = {}
-start = 1500
-# initial value 
-with open('nfl_teams.csv', 'r') as in_file:
-    reader = csv.reader(in_file)
-    # skip header
-    next(reader)
-    for row in reader:
-            scores[row[1]] = start
-# NFL teams list for SQL
 
 def rating(func_dict):
     log_dict = {}
@@ -59,7 +45,7 @@ def rating(func_dict):
         new_value = (new_max - new_min) / (max_num - min_num) * (value - max_num) + 5
         new_value = round(new_value)
         test_dict[key] = new_value
-    return(test_dict, log_dict)
+    return(test_dict)
     # function to convert the data from large values to rating out of 5 via log10 and data re_scaling
 
 def rating_change(winner, loser):
@@ -86,105 +72,6 @@ def ties(team1, team2):
 def carry_over(rating):
     new_rating = (0.75 * rating) + (0.25 * 1500)
     return new_rating
-
-with open('2020fixtures.csv', 'r') as in_file:
-    reader = csv.reader(in_file)
-    # skip header
-    next(reader)
-    for row in reader:
-        if row[5] == row[6] and row[3] == '@':
-            temp_rating = scores[row[4]] + 100
-            update = ties(scores[row[2]], temp_rating)
-            scores.update({row[2]:update[0]})
-            scores.update({row[4]:update[1]})
-        elif row[5] == row[6] and row[3] != '@':
-            temp_rating = scores[row[2]] + 100
-            update = ties(temp_rating, scores[row[4]])
-            scores.update({row[2]:update[0]})
-            scores.update({row[4]:update[1]})
-        elif row[3] == '@':
-            temp_rating = scores[row[4]] + 100
-            update = rating_change(scores[row[2]], temp_rating)
-            scores.update({row[2]:update[0]})
-            scores.update({row[4]:update[1]})
-        else:
-            temp_rating = scores[row[2]] + 100
-            update = rating_change(temp_rating, scores[row[4]])
-            scores.update({row[2]:update[0]})
-            scores.update({row[4]:update[1]})
-        # temp_rating includes 100 to take into account home field advantage
-# NFL 2020 regular and playoff season results for ranking calc + then SQL insert
-
-for key, value in scores.items():
-    change = carry_over(value)
-    scores.update({key:change})
-    # 2020 carry_over
-
-with open('2021fixtures.csv', 'r') as in_file:
-    reader = csv.reader(in_file)
-    # skip header
-    next(reader)
-    for row in reader:
-        if row[5] == row[6] and row[3] == '@':
-            temp_rating = scores[row[4]] + 100
-            update = ties(scores[row[2]], temp_rating)
-            scores.update({row[2]:update[0]})
-            scores.update({row[4]:update[1]})
-        elif row[5] == row[6] and row[3] != '@':
-            temp_rating = scores[row[2]] + 100
-            update = ties(temp_rating, scores[row[4]])
-            scores.update({row[2]:update[0]})
-            scores.update({row[4]:update[1]})
-        elif row[3] == '@':
-            temp_rating = scores[row[4]] + 100
-            update = rating_change(scores[row[2]], temp_rating)
-            scores.update({row[2]:update[0]})
-            scores.update({row[4]:update[1]})
-        else:
-            temp_rating = scores[row[2]] + 100
-            update = rating_change(temp_rating, scores[row[4]])
-            scores.update({row[2]:update[0]})
-            scores.update({row[4]:update[1]})
-# NFL 2021 regular and playoff season results for ranking calc + then SQL insert
-
-for key, value in scores.items():
-    change = carry_over(value)
-    scores.update({key:change})
-    # 2021 carry_over
-
-# break here or move 2022 data for the scores page - see note at the bottom about SQL
-
-with open('2022fixtures.csv', 'r') as in_file:
-    reader = csv.reader(in_file)
-    # skip header
-    next(reader)
-    for row in reader:
-        if int(row[0]) < 10:
-            if row[6] == row[7] and row[4] == '@':
-                temp_rating = scores[row[5]] + 100
-                update = ties(scores[row[3]], temp_rating)
-                scores.update({row[3]:update[0]})
-                scores.update({row[5]:update[1]})
-            elif row[6] == row[7] and row[4] != '@':
-                temp_rating = scores[row[3]] + 100
-                update = ties(temp_rating, scores[row[5]])
-                scores.update({row[3]:update[0]})
-                scores.update({row[5]:update[1]})
-            elif row[4] == '@':
-                temp_rating = scores[row[5]] + 100
-                update = rating_change(scores[row[3]], temp_rating)
-                scores.update({row[3]:update[0]})
-                scores.update({row[5]:update[1]})
-            else:
-                temp_rating = scores[row[3]] + 100
-                update = rating_change(temp_rating, scores[row[5]])
-                scores.update({row[3]:update[0]})
-                scores.update({row[5]:update[1]})
-        else:
-            break
-# 2022 schedule
-
-print(rating(scores))
 
     
 #print(teams_response.status_code)
